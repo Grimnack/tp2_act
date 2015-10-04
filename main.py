e9e2f2c66f9a7fac4228788e029dc114ca0243ae
Grimnack <matthieu.caron@laposte.net>
2015-10-04 18:09:47 +0200

    q6 dans main.py

--- main.py
+++ main.py
@@ -4,14 +4,42 @@ Tp diviser pour regner de Matthieu Caron et Armand Bour
 
 '''
 
-'''
-   Fusion de lignes de toit
-   Premier point c est simple on prend le plus petit abscisse.
-   En suite on cherche à monter toujours plus haut,
-   On compare donc le y avec le y du dernier point de la 
 
+def fusion(Liste1,Liste2):
 '''
-def fusion(Liste1,Liste2,res):
-	#cas init
-	if res == [] :
-		
+   Fusion de lignes de toits
+'''
+	res = []
+    x1 = 0
+    x2 = 0
+    hauteur1 = -1 #valeur impossible
+    hauteur2 = -1 #valeur impossible
+    #premiere fois on dois ajouter le premier point
+    if Liste1[0][0] < Liste2[0][0] :
+        res.append(Liste1[0][0])
+        hauteur1 = Liste1[0][1]
+        x1++
+    else :
+        res.append(Liste2[0][0])
+        hauteur2 = Liste2[0][1]
+        x2++
+    while (x1<len(Liste1) && x2<len(Liste2)) :
+        #on doit choisir un des deux éléments de la liste
+        #donc celui avec le plus petit abscisse
+        if(Liste1[x1][0] < Liste2[x2][0]) :
+            #on selectionne l'element de Liste1[x1]
+            if hauteur2 > Liste1[x1][1] :
+
+            else :
+                res.append(Liste1[x1])
+                hauteur1 = Liste1[x1][1]
+                x1++
+        else :
+            #on selectionne l'element Liste2[x2]
+            if hauteur1 > Liste2[x2][1] :
+
+            else :
+                res.append(Liste2[x2])
+                hauteur2 = Liste2[x2][1]
+                x2++
+
