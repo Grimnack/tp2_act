9c13159bcf9ed2a60ce0c841c781931fbb252531
Armand (Tydax) Bour <tydaxx@gmail.com>
2015-10-07 11:27:22 +0200

    Corrigé quelques fautes de frappe

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 
 from random import * 
 '''
-Tp diviser pour regner de Matthieu Caron et Armand Bour
+TP n°2 d'Algorithmes et Complexité
+"Diviser pour régner"
+
+Matthieu CARON
+Armand BOUR
+
 '''
 
 
@@ -14,10 +19,10 @@ def fusion(Liste1,Liste2):
     res = []
     x1 = 0
     x2 = 0
-    hauteur1 = -1 #valeur impossible
-    hauteur2 = -1 #valeur impossible
-    ligneDessus = -1 #valeur impossible qui sera plus tard soit 1 soit 2
-    #premiere fois on dois ajouter le premier point
+    hauteur1 = -1 # Valeur impossible
+    hauteur2 = -1 # Valeur impossible
+    ligneDessus = -1 # Valeur impossible qui sera plus tard soit 1 soit 2
+    # Première fois on dois ajouter le premier point
     if Liste1[0][0] < Liste2[0][0] :
         res.append(Liste1[0])
         hauteur1 = Liste1[0][1]
@@ -28,11 +33,11 @@ def fusion(Liste1,Liste2):
         hauteur2 = Liste2[0][1]
         ligneDessus = 2
         x2 = x2 + 1
-    while (x1<len(Liste1) and x2<len(Liste2)) :
-        #on doit choisir un des deux éléments de la liste
-        #donc celui avec le plus petit abscisse
+    while (x1 < len(Liste1) and x2 < len(Liste2)) :
+        # On doit choisir un des deux éléments de la liste
+        # Donc celui avec la plus petite abscisse
         if(Liste1[x1][0] < Liste2[x2][0]) :
-            #on selectionne l'element de Liste1[x1]
+            # On sélectionne l'élément de Liste1[x1]
             if hauteur2 > Liste1[x1][1] :
                 if ligneDessus == 2 :
                     hauteur1 = Liste1[x1][1]
@@ -47,7 +52,7 @@ def fusion(Liste1,Liste2):
                 ligneDessus = 1
                 x1 = x1 + 1
         else :
-            #on selectionne l'element Liste2[x2]
+            # On sélectionne l'élément Liste2[x2]
             if hauteur1 > Liste2[x2][1] :
                 if ligneDessus == 1:
                     hauteur2 = Liste2[x2][1]
@@ -61,10 +66,10 @@ def fusion(Liste1,Liste2):
                 hauteur2 = Liste2[x2][1]
                 ligneDessus = 2
                 x2 = x2 + 1
-    #A partir d'ici on a probablement une des deux liste qui n'a pas été entièrement parcourue
-    while x1 <len(Liste1):
+    # A partir d'ici on a probablement une des deux listes qui n'a pas été entièrement parcourue
+    while x1 < len(Liste1):
         res.append(Liste1[x1])
-        x1 = x1 +1
+        x1 = x1 + 1
     while x2 < len(Liste2) :
         res.append(Liste2[x2])
         x2 = x2 + 1
@@ -80,16 +85,16 @@ def fusion(Liste1,Liste2):
 
 # print fusion(L1,L2)
 
-def creationLigne (listeBatiments,debut,fin) :
+def creationLigne(listeBatiments,debut,fin) :
     '''
-        Premiere version de création de ligne, utilisant la strategie 
-        diviser pour regner. 
+        Première version de création de ligne, utilisant la stratégie 
+        diviser pour régner. 
         Version récursive non terminale
     '''
-    #l'idée de diviser pour regner
-    #couper le problème en sous problème pour que ce soit plus simple à résoudre
-    #Il est simple de créer un ligne de toit à partir d'un seul batiment 
-    #mais aussi si on a pas de batiment : ligne vide
+    # L'idée de diviser pour régner :
+    # Découper le problème en sous-problèmes pour que celui-ci soit plus simple à résoudre
+    # Il est simple de créer un ligne de toit à partir d'un seul bâtiment 
+    # Mais aussi si on a pas de bâtiment : ligne vide
     #if listeBatiments == [] :
     if debut > fin :
         return []
